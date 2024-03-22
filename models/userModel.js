8d30bb42457a46b2fbf6cad7383d0e79579bf1bd
Asim Rana <asimranaxo@gmail.com>
2024-03-23 00:47:23 +0530

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: [true, "Email already taken"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+}, { 
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
